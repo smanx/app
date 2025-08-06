990a7d9aac1bfec9054450b747104b6896783a08
cc <mail>
2025-08-06 15:26:31 +0800

    save

---
+++ files/other/ffandown-missav.js
@@ -0,0 +1,13 @@
+class Parser {
+    match(url) {
+        // return true if the url is matched
+        return url.startsWith('https://missav');
+    }
+    async parser(url, options) {
+        return {
+            url: 'http://playertest.longtailvideo.com/adaptive/bipbop/gear4/prog_index.m3u8',
+            headers: [
+            ]
+        }
+    }
+}
